d7e189c6eed6d14e0cbefb475d1e89f9ab6d38fe
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-04 20:27:39 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,14 +46,7 @@ function setup() {
 
 function draw() {
   background(220);
-  Engine.update(engine);
-  strokeWeight(2);
-  stroke(0);
 
-  // 先畫出攝影機畫面
-  image(video, 0, 0, width, height);
-
-  // 在畫面最上方中間顯示標題文字（移到這裡）
   push();
   textAlign(CENTER, TOP);
   textSize(32);
@@ -65,35 +58,6 @@ function draw() {
   fill(30, 30, 30);
   text("淡江教育科技系", width / 2, 10);
   pop();
-
-  if (random() < 0.1) {
-    circles.push(new Circle());
-  }
-
-  for (let i=circles.length-1; i>=0; i--) {
-    circles[i].checkDone();
-    circles[i].display();
-
-    if (circles[i].done) {
-      circles[i].removeCircle();
-      circles.splice(i, 1);
-    }
-  }
-
-  if (hands.length > 0) {
-    let thumb = hands[0].keypoints[THUMB_TIP];
-    let index = hands[0].keypoints[INDEX_FINGER_TIP];
-    fill(0, 255, 0);
-    noStroke();
-    circle(thumb.x, thumb.y, 10);
-    circle(index.x, index.y, 10);
-
-    bridge.bodies[0].position.x = thumb.x;
-    bridge.bodies[0].position.y = thumb.y;
-    bridge.bodies[bridge.bodies.length-1].position.x = index.x;
-    bridge.bodies[bridge.bodies.length-1].position.y = index.y;
-    bridge.display();
-  }
 }
 
 // Callback function for when handPose outputs data
